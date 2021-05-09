834e08010d7c130b259d3a8b167c5fe302c782b2
mateusntn <mateusantoniodev@gmail.com>
2021-05-09 02:18:00 -0300

    Create INSERT function

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -20,6 +20,19 @@ namespace GUI
 
         private void btnSend_Click(object sender, EventArgs e)
         {
+                MySqlConnection connection = new MySqlConnection(connectionString: "server=localhost;user id=root;pwd=7890;database=duas_camadas;allowuservariables=True");
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(cmdText: $"INSERT INTO laptops (brand, ram, storage) values ('{txtBrand.Text}', '{txtRam.Text}', '{txtStorage.Text}')", connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(text: "Registro salvo com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(text: "Ocorreu um erro ao tentar salvar.");
+            }
+            finally { connection.Close(); }
             
         }
     }
