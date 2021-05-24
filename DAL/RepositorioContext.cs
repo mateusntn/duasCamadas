302876e114ade89b97a25f64f6bc0a114c294f0b
mateusntn <mateusantoniodev@gmail.com>
2021-05-24 12:07:27 -0300

    Create Repositorio Context

---
+++ DAL/RepositorioContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class RepositorioContext : DbContext
+    {
+        public RepositorioContext(): base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=WINAPVUUCDEC8EK\SQLEXPRESS;Initial Catalog=duasCamadas;Integrated Security=True");
+        }
+        public DbSet<Laptop> Laptops { get; set; }
+    }
+}
