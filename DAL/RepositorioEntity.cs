4aae050f36f80dac23c0f3536651a131d0f42f56
mateusntn <mateusantoniodev@gmail.com>
2021-05-24 16:14:47 -0300

    Changes the create and consult method of the RepositorioEntity

--- DAL/RepositorioEntity.cs
+++ DAL/RepositorioEntity.cs
@@ -9,14 +9,16 @@ namespace DAL
 {
     public class RepositorioEntity : IRepositorio
     {
+        private RepositorioContext db = new RepositorioContext();
         public Laptop Consult(string brand)
         {
-            throw new NotImplementedException();
+            return db.Laptops.First<Laptop>(c => c.Brand == brand);
         }
 
         public void Create(Laptop laptop)
         {
-            throw new NotImplementedException();
+            db.Laptops.Add(laptop);
+            db.SaveChanges();
         }
 
         public void Delete(string brand)
