96cb5fb65e4bca219168698fa24eb7b953fb531f
mateusntn <mateusantoniodev@gmail.com>
2021-05-24 15:52:08 -0300

    Creates TesteDBEntity

---
+++ TestLaptop/TestDBEntity.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLaptop
+{
+    [TestClass]
+    public class TestDBEntity
+    {
+        [TestMethod]
+        public void CreateLaptop()
+        {
+            IRepositorio rep = new RepositorioEntity();
+            try
+            {
+                rep.Create(new Laptop("Dell Latitude 5410", 16, 500));
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Assert.IsNotNull(rep.Consult("Dell Latitude 5410"));
+        }
+    }
+}
