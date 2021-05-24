1d740d7095f19878eef70b040db015cc333bfa2c
mateusntn <mateusantoniodev@gmail.com>
2021-05-24 12:07:11 -0300

    Create RepositorioEntity

---
+++ DAL/RepositorioEntity.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class RepositorioEntity : IRepositorio
+    {
+        public Laptop Consult(string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Laptop laptop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Laptop> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
